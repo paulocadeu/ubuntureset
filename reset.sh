5f3c1451f452ac478ad1da7bc713ee5cb01183e1
aluno <paulocadeu@gmail.com>
2019-09-03 14:40:34 -0300

    whatever

--- reset.sh
+++ reset.sh
@@ -1,12 +1,13 @@
+echo "Iniciando o reset deste usuario"
+
 cd
 rm -rf * .*
-
 xdg-user-dirs-update
 xdg-user-dirs-update --force
-
 cp /etc/skel/.bashrc ~/.bashrc
-
 source ~/.bashrc
+
+gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 
 echo "Reset concluido"
 
