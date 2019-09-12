45a260a9d95ef8a437231a0f3f9447825f2b3c1b
paulocadeu <paulocadeu@gmail.com>
2019-09-12 16:14:38 -0300

    a

--- reset.sh
+++ reset.sh
@@ -5,13 +5,16 @@ rm -rf * .*
 xdg-user-dirs-update
 xdg-user-dirs-update --force
 cp /etc/skel/.bashrc ~/.bashrc
-source ~/.bashrc
 
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 
 git clone https://github.com/paulocadeu/vimportable
 cd vimportable
 ./install.sh
+cd ..
+rm vimportable
+source ~/.bashrc
+
 
 echo "Reset concluido"
 
