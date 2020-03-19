17247278d6e2b0b113e7e0989a0b2b6bd243873c
Paulo Wagner Teixeira Cadeu de Oliveira <paulocadeu@localhost.localdomain>
2020-03-19 01:21:01 -0300

    Refatoração do codigo 18/03/2020

--- reset.sh
+++ reset.sh
@@ -1,21 +1,58 @@
-echo "Iniciando o reset deste usuario"
-
-cd
-rm -rf * .*
-xdg-user-dirs-update
-xdg-user-dirs-update --force
-cp /etc/skel/.bashrc ~/.bashrc
-
-gsettings set com.canonical.Unity.Launcher launcher-position Bottom
-
-git clone https://github.com/paulocadeu/vimportable
-cd vimportable
-./install.sh
-cd ..
-
-rm -rf vimportable
-source ~/.bashrc
-
-echo "Reset concluido"
-systemctl reboot
-
+#!/bin/bash
+#-------------------------------------------------------#
+#                       Variaveis                       #
+#-------------------------------------------------------#
+home=~/
+wallpaper="/wallpaper-inf-16x9.png"
+wallpaperDir="Imagens/Wallpapers"
+#-------------------------------------------------------#
+#                       Funcoes                         #
+#-------------------------------------------------------#
+function preWipe
+{
+    echo "------------Iniciando o reset deste usuario------------"
+    cp -r ./Wallpapers /tmp
+}
+function posWipe
+{
+    echo "--------------------Reset concluido--------------------"
+    #systemctl reboot
+    counter=$((10+0))
+    echo "Reiniciando o computador em "$counter
+    while [ $counter -gt $((0)) ];
+    do 
+        counter=$(($counter-1))
+        echo $counter
+        sleep 1
+    done
+    systemctl reboot
+}
+function setWallpaper
+{
+    echo "Restaurando Wallpaper"
+    cp -r /tmp/Wallpapers $home$wallpaperDir
+    gsettings set org.gnome.desktop.background picture-uri file://$home$wallpaperDir$wallpaper
+}
+function setHorizontalLauncher
+{
+    echo "Ajustando Barra de Tarefas"
+    gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+}
+function wipeUser
+{
+    echo "Limpando Pastas"
+    cd $home
+    rm -rf *
+    xdg-user-dirs-update
+    xdg-user-dirs-update --force
+    cp /etc/skel/.bashrc ~/.bashrc
+    source ~/.bashrc
+}
+#-------------------------------------------------------#
+#                          Main                         #
+#-------------------------------------------------------#
+preWipe
+wipeUser
+setHorizontalLauncher
+setWallpaper
+posWipe
